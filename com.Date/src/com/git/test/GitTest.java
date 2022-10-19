9e48d3ddfce510739a42b2fc8c23ebc048938ba4
Priyanka H Patil <priyankahp4012@gmail.com>
2022-10-19 14:06:13 +0530

    commit to division

--- com.Date/src/com/git/test/GitTest.java
+++ com.Date/src/com/git/test/GitTest.java
@@ -1,5 +1,11 @@
 package com.git.test;
 
 public class GitTest {
-
+public static void main(String[] args) {
+	String arr="i am priyanka patil";
+	String [] arr2= arr.split(" ");
+	for (int i = 0; i < arr2.length; i++) {
+		System.out.println(arr2[i]);
+	}
+}
 }
